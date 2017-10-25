1726eac52516e3d4520ac2dbdbec046b7e383a21
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-10-25 18:11:18 +0000

    User can Login and Logout, Add login, logout and signup links to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?, :authentication_required
 
   def current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authentication_required
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:message] = "You are logged in successfully!"
+      redirect_to root_path
+    else
+      flash[:message] = "Invalid Email or Password."
+      render :new
+    end
+
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
